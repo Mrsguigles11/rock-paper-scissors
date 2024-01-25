2481c0006465e64de3b925f18f98a4d8d358d4e3
Harry Woodgate <harry_woodgate11@hotmail.co.uk>
2024-01-25 11:46:48 +0000

    Add the completed and working getComputerChoice function.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Function for computer //
 
+function getComputerChoice() { const randomNum = Math.floor(Math.random() * 3) + 1; 
+if (randomNum === 1) {
+    return("Rock") } 
+else if (randomNum === 2) {
+    return("Paper") }
+else return("Scissors") }
