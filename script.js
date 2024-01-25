d512142f8a7c148b30928fbc6814f45c15e74103
Harry Woodgate <harry_woodgate11@hotmail.co.uk>
2024-01-25 12:35:46 +0000

    Add the function playRound with working script for if player chooses rock.

--- script.js
+++ script.js
@@ -2,7 +2,26 @@
 
 function getComputerChoice() { const randomNum = Math.floor(Math.random() * 3) + 1; 
 if (randomNum === 1) {
-    return("Rock") } 
+    return("rock") } 
 else if (randomNum === 2) {
-    return("Paper") }
-else return("Scissors") }
+    return("paper") }
+else return("scissors") }
+
+
+// Function for playing a round //
+
+function playRound(playerSelection, computerSelection) { playerSelection = playerSelection.toLowerCase();
+if (playerSelection === "rock" && computerSelection === "rock") {
+    return("It's a draw!")} 
+else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return("You Win! Rock beats scissors!") }
+else if (playerSelection === "rock" && computerSelection === "paper") {
+    return("You lose! Paper beats rock!")
+}
+    
+}
+
+const playerSelection = "RoCK" ;
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection))
